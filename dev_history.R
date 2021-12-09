c9a8910ba81b53ae83fdfcb4d3ed587ca6a78c24
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-12-09 22:01:48 +0100

    initial commit

---
+++ dev_history.R
@@ -0,0 +1,15 @@
+## 2021-12-09 ====
+
+## tinytest
+# tinytest::setup_tinytest(pkgdir = ".")
+
+## license and readme
+# usethis::use_readme_rmd()
+# usethis::use_mit_license(copyright_holder = "Hendrik Wagenseil")
+
+## dev_history.R
+# usethis::use_build_ignore("dev_history.R")
+# usethis::edit_file("dev_history.R")
+
+## create package
+# usethis::create_package("~/Documents/repos/github/aRts")
