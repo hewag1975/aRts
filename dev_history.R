5c3a83c984e6465780e6e09058b9ae06cd033a13
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-12-09 22:27:39 +0100

    render readme, add first vignette, update description

--- dev_history.R
+++ dev_history.R
@@ -1,15 +1,18 @@
 ## 2021-12-09 ====
 
+## add vignette painting
+usethis::use_article("painting")
+
 ## tinytest
-# tinytest::setup_tinytest(pkgdir = ".")
+tinytest::setup_tinytest(pkgdir = ".")
 
 ## license and readme
-# usethis::use_readme_rmd()
-# usethis::use_mit_license(copyright_holder = "Hendrik Wagenseil")
+usethis::use_readme_rmd()
+usethis::use_mit_license(copyright_holder = "Hendrik Wagenseil")
 
 ## dev_history.R
-# usethis::use_build_ignore("dev_history.R")
-# usethis::edit_file("dev_history.R")
+usethis::use_build_ignore("dev_history.R")
+usethis::edit_file("dev_history.R")
 
 ## create package
 # usethis::create_package("~/Documents/repos/github/aRts")
