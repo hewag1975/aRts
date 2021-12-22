edea4d27e3a746b334df6a07f97c51314e1a0489
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-12-22 10:20:24 +0100

    add more test datasets

--- data-raw/pic.R
+++ data-raw/pic.R
@@ -1,9 +1,17 @@
 ## code to prepare sample datasets goes here
 
 ## autumn
-ifl = "~/Pictures/images/pic_001.jpg"
+ifl = "~/Pictures/images/autumn.jpg"
 autumn = stars::read_stars(ifl)
 names(autumn) = "Autumn forest"
 sf::st_crs(autumn) = 3035
 
 usethis::use_data(autumn, overwrite = TRUE)
+
+## mountains
+ifl = "~/Pictures/images/mountains_2.jpg"
+mountains = stars::read_stars(ifl)
+names(mountains) = "Mountains"
+sf::st_crs(mountains) = 3035
+
+usethis::use_data(mountains, overwrite = TRUE)
