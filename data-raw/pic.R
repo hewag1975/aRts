8d2b2ca67c0e0c9adb3d99763a3b3805087d906e
hendrik wagenseil <hendrik.wagenseil@metergroup.com>
2021-12-25 23:28:36 +0100

    work on vignette

--- data-raw/pic.R
+++ data-raw/pic.R
@@ -1,17 +1,10 @@
 ## code to prepare sample datasets goes here
 
-## autumn
-ifl = "~/Pictures/images/autumn.jpg"
-autumn = stars::read_stars(ifl)
-names(autumn) = "Autumn forest"
-sf::st_crs(autumn) = 3035
-
-usethis::use_data(autumn, overwrite = TRUE)
-
 ## mountains
-ifl = "~/Pictures/images/mountains_2.jpg"
-mountains = stars::read_stars(ifl)
-names(mountains) = "Mountains"
-sf::st_crs(mountains) = 3035
-
-usethis::use_data(mountains, overwrite = TRUE)
+## not used, included images in inst/img instead
+# ifl = "~/Pictures/images/mountains_2.jpg"
+# mountains = stars::read_stars(ifl)
+# names(mountains) = "Mountains"
+# sf::st_crs(mountains) = 3035
+#
+# usethis::use_data(mountains, overwrite = TRUE)
